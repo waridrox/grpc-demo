6ece90896c19a8019f9dc07a42cb7fb1df49892c
waridrox <mohdwarid4@gmail.com>
2022-03-06 22:35:14 +0530

    creating new entry and reading all todos

--- server.js
+++ server.js
@@ -4,8 +4,33 @@ const packageDef = protoLoader.loadSync("todo.proto", {}); //file name as first 
 
 const grpcObject = grpc.loadPackageDefinition(packageDef)
 
-const todoPackage = grpc.todoPackage;
+const todoPackage = grpcObject.todoPackage;
 
 const server = new grpc.Server();
-server.bind("0.0.0.0:40000", grpc.ServerCredentials.createInsecure())
+server.bind("0.0.0.0:40000", grpc.ServerCredentials.createInsecure());
 
+server.addService(todoPackage.Todo.service, {
+    "createTodo": createTodo,
+    "readTodos": readTodos,
+});
+
+server.start();
+
+const todos = []
+
+function createTodo(call, callback) {
+
+    const todoItem = {
+        "id": todos.length + 1,
+        "text": call.request.text
+    }
+    todos.push(todoItem)
+    callback(null, todoItem)
+    // console.log(call);
+}
+
+function readTodos(call, callback) {
+    // callback(null, todos) //we cannot send the whole array like this because it is expecting 
+    // an item of TodoItems
+    callback(null, { "items": todos})
+}
